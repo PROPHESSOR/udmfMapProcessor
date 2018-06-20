c34d130040e68ea657ffc460705e1494ba71765f
PROPHESSOR <xenteze@gmail.com>
2018-06-20 22:15:44 +0300

    Базовая скриптовая система

--- index.js
+++ index.js
@@ -8,8 +8,13 @@
 const IN_FILE = 'test/test.udmf';
 const OUT_FILE = 'test/test.out.udmf';
 
+const fs = require('fs');
+const path = require('path');
+
 const Line = require('./structures/Line');
 const Thing = require('./structures/Thing');
+const Script = require('./Script');
+
 const { udmf2json, json2udmf, jsonCompress, jsonDecompress } = require('./udmf2json');
 
 const file = udmf2json(IN_FILE, OUT_FILE);
@@ -20,7 +25,7 @@ for (const block of file) {
     if (block[0] === 'linedef') {
         const line = new Line(block[1], file);
         lines.push(line);
-        console.log(line)
+        // console.log(line)
     }
 }
 
@@ -36,13 +41,26 @@ for (const line of lines) {
                 || line.sideback.texturemiddle === texture
                 || line.sideback.texturebottom === texture
             ))) {
-            console.log(1);
             const lineAvg = line.avg();
             file.push((new Thing(lineAvg, 56)).toArray());
         }
     }
 }
 
-console.dir(jsonCompress(jsonDecompress(file)));
+const scripts = [];
+{
+    const scriptpath = path.join(__dirname, 'scripts');
+    let folderdata = null;
+    try {
+        folderdata = fs.readdirSync(scriptpath);
+    } catch(e) {
+        console.error(e);
+        return console.error(`\n\nНевозможно прочитать содержимое папки по пути ${scriptpath}!\n`)
+    }
+
+    for(const scriptfolder of folderdata) {
+        scripts.push(new Script(path.join(scriptpath, scriptfolder)));
+    }
+}
 
 json2udmf(jsonCompress(jsonDecompress(file)), OUT_FILE);
