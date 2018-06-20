a3d849b0cd7942bf21fe78c841f92d993cf1c80e
PROPHESSOR <xenteze@gmail.com>
2018-06-20 21:43:46 +0300

    Добавлен пример скрипта

---
+++ scripts/example/script.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2018 PROPHESSOR
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+'use strict';
+
+module.exports = function() {
+	// Здесь будет код скрипта
+}
